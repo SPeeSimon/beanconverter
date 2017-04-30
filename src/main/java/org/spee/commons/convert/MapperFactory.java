88db97d0f00c42e3e20a7c5dd4ebde27c32bdbba
Haverdings <shaverdings@hotmail.com>
2017-04-30 15:54:48 +0200

    cleanup

--- src/main/java/org/spee/commons/convert/MapperFactory.java
+++ src/main/java/org/spee/commons/convert/MapperFactory.java
@@ -14,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.spee.commons.convert.generator.BeanCreationStrategy;
 import org.spee.commons.convert.internals.MappingLocator;
+import org.spee.commons.utils.CollectionUtils;
 import org.spee.commons.utils.ReflectionUtils;
 
 import com.google.common.base.Preconditions;
@@ -30,7 +31,9 @@ public class MapperFactory {
 	
     static{
     	register(DefaultConverters.class);
+    	register(CollectionUtils.class);
     }
+
     
 	private static <S,T> Convert<S,T> createTypeConverter(Class<S> sourceType, Class<T> targetType) {
 		Preconditions.checkArgument(!sourceType.isPrimitive(), "Primitive type not supported: %s", sourceType);
@@ -56,53 +59,16 @@ public class MapperFactory {
 		}
 	}    
     
-    public <S, T> T convert(S from, Class<T> toClass){
-    	return createTypeConverter((Class<S>)from.getClass(), toClass).convert(from);
+
+	@SuppressWarnings("unchecked")
+	public <S, T> T convert(S from, Class<T> toClass){
+    	return getConverter((Class<S>)from.getClass(), toClass).convert(from);
     }
 
     
-
-    void convertCreatingNew(Object from, Class<Object> toClass){
-    	try {
-			Object to = toClass.newInstance();
-		} catch (InstantiationException | IllegalAccessException e) {
-			throw new RuntimeException(e);
-		}
+    public static <S,T> Convert<S,T> getConverter(final Class<S> fromClass, final Class<T> toClass){
+    	return createTypeConverter(fromClass, toClass);
     }
-    
-    
-    public static <S,T> com.google.common.base.Converter<S, T> getConverter(final Class<S> fromClass, final Class<T> toClass){
-    	try {
-			return new com.google.common.base.Converter<S, T>() {
-				final MethodHandle doForward, doBackward;
-				{
-					doForward = MappingLocator.bootstrap(MethodHandles.publicLookup(), "convert", MethodType.methodType(toClass, fromClass)).getTarget();
-					doBackward = MappingLocator.bootstrap(MethodHandles.publicLookup(), "convert", MethodType.methodType(fromClass, toClass)).getTarget();
-				}
-				
-				@Override
-				protected T doForward(S a) {
-					try {
-						return (T)doForward.invokeWithArguments(a);
-					} catch (Throwable e) {
-						throw new RuntimeException(e);
-					}
-				}
-
-				@Override
-				protected S doBackward(T b) {
-					try {
-						return (S)doBackward.invokeWithArguments(b);
-					} catch (Throwable e) {
-						throw new RuntimeException(e);
-					}
-				}
-			};
-		} catch (NoSuchMethodException | IllegalAccessException e) {
-			throw new RuntimeException(e);
-		}
-    }
-
 
 
 	public static void register(String id, com.google.common.base.Converter<?, ?> customConverter) {
@@ -125,7 +91,6 @@ public class MapperFactory {
 		for (Method method : container.getMethods()) {
 			if( methodCheck.apply(method) ){
 				logger.debug("Registering @Converter on method {} of {}", method, container);
-				
 				try {
 					Class<?> sourceType = method.getParameterTypes()[0];
 					Class<?> targetType = method.getReturnType();
@@ -135,7 +100,6 @@ public class MapperFactory {
 				} catch (IllegalAccessException e) {
 					e.printStackTrace();
 				}
-				
 			}
 		}
 	}
